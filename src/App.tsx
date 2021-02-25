86300e83dc873920c89210987c92923f8eac2466
Max Ivanov <waxivanov@gmail.com>
2021-02-25 15:56:10 +0200

    get data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+export type ProductItemType = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  amount: number;
+};
 
 const App = () => {
-  return (
-    <div>
-      <h1>fdfd</h1>
-    </div>
-  );
+  const getProducts = (): Promise<ProductItemType[]> =>
+    fetch('https://fakestoreapi.com/products')
+      .then((data) => data.json())
+      .then((products) => products)
+      .catch((error) => console.log(error.message));
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return <h1 style={{ backgroundColor: 'red' }}>App</h1>;
 };
 
 export default App;
